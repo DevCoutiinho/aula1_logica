1db6d476107a896dd04ba04a94a65823a0f5ed8b
Marcos Coutinho <veniciusm4@gmail.com>
2024-11-28 11:09:31 -0300

    Add files via upload

---
+++ exemplo.py
@@ -0,0 +1,25 @@
+# idade = 39
+# print(idade)
+
+#int
+# idade = 39
+
+#float
+# altura = 1.85
+# preco = 10.0
+
+#string
+# nome = "Marcos"
+# endereco = "Rua Boa Ventura"
+
+# nome = input("Digite seu nome: ")
+
+# print("Olá,", nome)
+
+base = float(input("Digite a base do seu triangulo: "))
+
+altura = float(input("Digite a altura do seu triangulo: "))
+
+area = (f'A base do seu triangulo é: {base * altura}. ')
+
+print(area)
